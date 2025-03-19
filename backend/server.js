f4281459f93a1192f6412c8e7bf5ddc243a1a62d
Nilesh Wani <108217005+nileshnwani@users.noreply.github.com>
2025-03-19 06:06:04 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -27,10 +27,10 @@ const __dirname1 = path.resolve();
 //const __dirname1="https://github.com/nileshnwani/N-connect/blob/main";
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+  app.use(express.static("/frontend/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+    res.sendFile(path.resolve("frontend", "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
