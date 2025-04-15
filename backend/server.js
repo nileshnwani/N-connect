716e002aa50f03ae914e11d30a06926984be55f0
Nilesh Wani <108217005+nileshnwani@users.noreply.github.com>
2025-04-16 00:08:21 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -23,12 +23,12 @@ app.use("/api/message", messageRoutes);
 
 // --------------------------deployment------------------------------
 
-const __dirname1 = path.resolve();
+const __dirname1 = "/opt/render/project/";
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "../frontend/build")));
+  app.use(express.static(path.join(__dirname1, "frontend/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "../frontend", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
